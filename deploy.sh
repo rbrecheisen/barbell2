ed29624c28d0232a9ba2ccb13eb15f9bf052454f
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2020-12-05 07:32:39 +0100

    Saving version 0.1.0 before deploying to PyPI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export CMD=${1}
 if [ "${CMD}" == "" ]; then
   export CMD=minor
@@ -7,15 +6,22 @@ fi
 docker run --rm -it -v $(pwd):/app -w /app treeder/bump --filename VERSION ${CMD}
 export VERSION=$(cat VERSION)
 echo ""
-echo "Is this the right version? Type "yes" to continue packaging and upload to PyPI, or any other key to quit."
+echo "Is this the right version? Type "yes" to continue, or any other key to quit."
 read line
 if [ "${line}" != "yes" ]; then
   exit 0
 fi
+git status
+echo "Everything ready to be pushed to Git? Type "yes" to continue, or any other key to quit."
+read line
+if [ "${line}" != "yes" ]; then
+  exit 0
+fi
+git add -A
+git commit -m "Saving version ${VERSION} before deploying to PyPI"
+git push
 python setup.py sdist bdist_wheel
 export TWINE_USERNAME=$(cat ${HOME}/pypiusername.txt)
-export TWINE_PASSWORD=$(cat ${HOME}/testpypipassword.txt)
-twine upload -r testpypi dist/*
 export TWINE_PASSWORD=$(cat ${HOME}/pypipassword.txt)
 twine upload dist/*
 
