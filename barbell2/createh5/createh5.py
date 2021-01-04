9af82da493a37c5ba938b9cd339938bf6c317691
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-01-04 12:00:53 +0100

    Saving version 0.21.0 before deploying to PyPI. Added extra checks for correct TAG labels

--- barbell2/createh5/createh5.py
+++ barbell2/createh5/createh5.py
@@ -11,6 +11,14 @@ from barbell2.lib.dicom import Dcm2Numpy, Tag2NumPy
 
 from sklearn.model_selection import train_test_split
 
+LOG = None
+
+
+def log(message):
+    global LOG
+    print(message)
+    LOG.write(str(message) + '\n')
+
 
 def info_requested(args):
     if len(args) == 2 and args[1] == '--info':
@@ -19,7 +27,7 @@ def info_requested(args):
 
 
 def show_info():
-    print("""The createh5.py script allows you to create HDF5 files for training, validation and testing your
+    log("""The createh5.py script allows you to create HDF5 files for training, validation and testing your
     deep learning networks in Keras.""")
 
 
@@ -48,7 +56,7 @@ def check_collections(root_dir, collections):
     for collection in collection_list:
         if not os.path.isdir(os.path.join(root_dir, collection)):
             raise MyException('Collection directory "{}" does not exist'.format(collection))
-    print('Done')
+    log('Done')
 
 
 def has_correct_dimensions(dcm_file, width, height):
@@ -61,7 +69,7 @@ def has_correct_dimensions(dcm_file, width, height):
 def collect_files(root_dir, collections, width, height):
     file_list = []
     collection_list = [x.strip() for x in collections.split(',')]
-    print(collection_list)
+    log(collection_list)
     for collection in collection_list:
         collection_dir = os.path.join(root_dir, collection)
         for root, dirs, files in os.walk(collection_dir):
@@ -73,11 +81,10 @@ def collect_files(root_dir, collections, width, height):
                         tag_file = os.path.join(root, f)[:-4] + '.tag'
                         if os.path.isfile(tag_file):
                             file_list.append([dcm_file, tag_file])
-                            print('Adding {} to collection'.format(dcm_file))
+                            log('Adding {} to collection'.format(dcm_file))
     return file_list
 
 
-# TODO: check that shuffling and train_test_split work correctly with a nested list file_list
 def shuffle_file_list(file_list):
     random.shuffle(file_list)
     return file_list
@@ -107,6 +114,8 @@ def update_labels(pixels):
     # Alberta protocol: AIR = 0, MUSCLE = 1, IMAT = 2, VAT = 5, SAT = 7
     # pixels[pixels == 0] = 0
     pixels[pixels == 2] = 0
+    pixels[pixels == 4] = 0
+    pixels[pixels == 11] = 0
     pixels[pixels == 12] = 0
     pixels[pixels == 14] = 0
     # pixels[pixels == 1] = 1
@@ -115,7 +124,24 @@ def update_labels(pixels):
     return pixels
 
 
+def labels_ok(labels):
+    for label in [0, 1, 2, 3]:
+        if label not in labels:
+            return False
+    return len(labels) == 4
+
+
+def update_label_counts(label_counts, labels):
+    for label in labels:
+        if label not in label_counts.keys():
+            label_counts[label] = 1
+        else:
+            label_counts[label] += 1
+    return label_counts
+
+
 def create_h5_from_file_list(file_list, output_file_path):
+    label_counts = {}
     with h5py.File(output_file_path, 'w') as h5f:
         count = 1
         for file_pair in file_list:
@@ -123,48 +149,22 @@ def create_h5_from_file_list(file_list, output_file_path):
             tag_pixels = get_tag_pixels(file_pair[1], dcm_pixels.shape)
             tag_pixels = update_labels(tag_pixels)
             labels = np.unique(tag_pixels)
+            if labels_ok(labels):
+                log('Labels not ok ({})'.format(labels))
+                continue
+            label_counts = update_label_counts(label_counts, labels)
             group = h5f.create_group('{:04d}'.format(count))
             group.create_dataset('images', data=dcm_pixels)
             group.create_dataset('labels', data=tag_pixels)
-            print('{:04d} added images and labels ({}) for {}'.format(count, labels, file_pair[0]))
+            log('{:04d} added images and labels ({}) for {}'.format(count, labels, file_pair[0]))
             count += 1
-    print('Done')
+    log('Done')
+    log('{}: {}'.format(output_file_path, label_counts))
 
 
-# def create_h5(root_dir, collections, width, height, output_file_path):
-#     file_list = []
-#     collection_list = [x.strip() for x in collections.split(',')]
-#     print(collection_list)
-#     for collection in collection_list:
-#         collection_dir = os.path.join(root_dir, collection)
-#         for root, dirs, files in os.walk(collection_dir):
-#             for f in files:
-#                 if f.endswith('.dcm') and not f.startswith('._'):
-#                     dcm_file = os.path.join(root, f)
-#                     # Only allow images to be included that have the correct dimension (e.g., 512x512)
-#                     if has_correct_dimensions(dcm_file, width, height):
-#                         tag_file = os.path.join(root, f)[:-4] + '.tag'
-#                         if os.path.isfile(tag_file):
-#                             file_list.append([dcm_file, tag_file])
-#                             print('Adding {} to collection'.format(dcm_file))
-#     print('Creating H5...')
-#     with h5py.File(output_file_path, 'w') as h5f:
-#         count = 1
-#         for file_pair in file_list:
-#             dcm_pixels = get_dcm_pixels(file_pair[0])
-#             tag_pixels = get_tag_pixels(file_pair[1], dcm_pixels.shape)
-#             group = h5f.create_group('{:04d}'.format(count))
-#             group.create_dataset('images', data=dcm_pixels)
-#             group.create_dataset('labels', data=tag_pixels)
-#             print('{:04d} added images and labels for {}'.format(count, file_pair[0]))
-#             count += 1
-#     print('Done')
-
-
-def run():
+def run(args):
 
     show_info()
-    args = get_args()
 
     # Verify that root folder exists and is not empty
     if not os.path.isdir(args.root_dir):
@@ -174,7 +174,7 @@ def run():
 
     # Verify that training, validation and test collections exist and are not empty
     if args.training is not None:
-        print('Checking collections training...')
+        log('Checking collections training...')
         check_collections(args.root_dir, args.training)
     if args.validation is not None:
         check_collections(args.root_dir, args.validation)
@@ -184,8 +184,6 @@ def run():
         check_collections(args.root_dir, args.split)
         if args.split_percentage is None:
             raise MyException('Argument --split_percentage is mandatory when choosing --split')
-
-    os.makedirs(args.output_dir, exist_ok=False)
 
     # Create training H5
     if args.training is not None:
@@ -223,15 +221,22 @@ def run():
         file_list = collect_files(args.root_dir, args.split, args.width, args.height)
         file_list = shuffle_file_list(file_list)
         training_files, validation_files = split_file_list(file_list, args.split_percentage)
+        log('>>> Creating training.h5...')
         create_h5_from_file_list(training_files, os.path.join(args.output_dir, args.output_file_name_training))
+        log('>>> Creating validation.h5...')
         create_h5_from_file_list(validation_files, os.path.join(args.output_dir, args.output_file_name_validation))
 
 
 def main():
+    args = get_args()
+    global LOG
+    os.makedirs(args.output_dir, exist_ok=False)
+    LOG = open(os.path.join(args.output_dir, 'output.txt'), 'w')
     try:
-        run()
+        run(args)
     except MyException as e:
-        print(e)
+        log(e)
+    LOG.close()
 
 
 if __name__ == '__main__':
--- tests/test_createh5.sh
+++ tests/test_createh5.sh
@@ -6,5 +6,7 @@ cd ../barbell2/createh5
 python createh5.py \
     /Volumes/USB_SECURE1/data/radiomics/projects/deepseg/data/mega/final \
     ${HOME}/Desktop/h5 \
+    --output_file_name_training=training.h5 \
+    --output_file_name_validation=validation.h5 \
     --split="FROGS, HEERLEN, MAASTRICHT_AKEN, NEWEPOC" \
     --split_percentage=0.8
