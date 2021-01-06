aede9a89bd059d5ab059b9e3ad24ab15cc3ee63a
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-01-06 09:36:22 +0100

    Added main module that lists available tools in the package

---
+++ barbell2/barbell2.py
@@ -0,0 +1,5 @@
+"""
+Main module.
+What should it do? Perhaps offer the user the possibility to see what's in the package?
+Showing a list of available tools? Like createh5, castorclient, dicomconverttoraw, dicomexplorer, etc.
+"""
