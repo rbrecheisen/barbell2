d0b324213f99f503930980c6e0399e4bd38f6f93
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-01-27 09:27:30 +0100

    Saving version 1.7.0 before deploying to PyPI.

--- barbell2/__init__.py
+++ barbell2/__init__.py
@@ -1,8 +1,10 @@
 """Top-level package for Barbell2."""
 
+import os
+
 __author__ = """Ralph Brecheisen"""
 __email__ = 'ralph.brecheisen@gmail.com'
-__version__ = '0.1.0'
+__version__ = os.environ['VERSION']
 
 from .dicomexplorer.dicomexplorer import DicomExplorer
 from .castorexportclient.castorexportclient import CastorExportClient
