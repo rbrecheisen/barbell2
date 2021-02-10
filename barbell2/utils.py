47079dcf493c58f3a12dcb86f6aff74e141364cb
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-02-10 10:31:23 +0100

    Added handy timing functions to utils

--- barbell2/utils.py
+++ barbell2/utils.py
@@ -1,4 +1,6 @@
 import os
+import time
+import math
 import unittest
 import datetime
 
@@ -45,3 +47,28 @@ class Logger(object):
 
     def __del__(self):
         self.f.close()
+
+
+def current_time_millis():
+    return int(round(time.time() * 1000))
+
+
+def current_time_secs():
+    return int(round(current_time_millis() / 1000.0))
+
+
+def elapsed_in_millis(start_time_millis):
+    return current_time_millis() - start_time_millis
+
+
+def elapsed_in_secs(start_time_secs):
+    return current_time_secs() - start_time_secs
+
+
+def duration(seconds):
+    h = int(math.floor(seconds/3600.0))
+    remainder = seconds - h * 3600
+    m = int(math.floor(remainder/60.0))
+    remainder = remainder - m * 60
+    s = int(math.floor(remainder))
+    return '{} hours, {} minutes, {} seconds'.format(h, m, s)
