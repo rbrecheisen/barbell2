5631681a791a136b721ed0fed4af0912e1ba0583
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-03-25 15:07:07 +0100

    Saving version 1.45.0 before deploying to PyPI. Fixed bug in dcm2npy

--- barbell2/dcm2npy.py
+++ barbell2/dcm2npy.py
@@ -25,17 +25,19 @@ class Dicom2Numpy:
         p = pydicom.dcmread(self.dcm_file_path)
         pixels = p.pixel_array
         self.npy_array = pixels.reshape(p.Rows, p.Columns)
-        if self.window is not None:
-            self.npy_array = apply_window(self.npy_array, self.window)
         if self.is_normalize_enabled():
             b = p.RescaleIntercept
             m = p.RescaleSlope
             self.npy_array = m * self.npy_array + b
+        if self.window is not None:
+            self.npy_array = apply_window(self.npy_array, self.window)
         return self.npy_array
 
 
 if __name__ == '__main__':
-    d2n = Dicom2Numpy('/Users/Ralph/Desktop/output-segmentl3/1.dcm')
+    from npy2png import Numpy2Png
+    d2n = Dicom2Numpy('/Volumes/GoogleDrive/My Drive/documents/projects/hpb/metabolicimaging/bodycomposition/20211109_demo_glasgow/pred/pancreas/4.dcm')
     d2n.set_window((400, 50))
-    pixels = d2n.execute()
-    print(pixels)
+    n2p = Numpy2Png(d2n.execute())
+    n2p.set_output_dir('/Volumes/GoogleDrive/My Drive/documents/projects/hpb/metabolicimaging/bodycomposition/20211109_demo_glasgow/pred/pancreas/')
+    n2p.execute()
