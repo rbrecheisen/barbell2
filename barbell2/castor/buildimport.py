c12645f2f7c2bd7a2263deb0a34232096a64949b
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-09-26 10:02:53 +0200

    Saving version 1.57.0 before deploying to PyPI.

--- barbell2/castor/buildimport.py
+++ barbell2/castor/buildimport.py
@@ -5,10 +5,10 @@ from barbell2.castor import builders
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('castor-export', help='Castor export Excel file')
-    parser.add_argument('dica-export', help='DHBA or DPCA export Excel data file')
-    parser.add_argument('dica-dd', help='DHBA or DPCA data dictionary')
-    parser.add_argument('output-dir', help='Output directory to store generated CSV files')
+    parser.add_argument('castor_export', help='Castor export Excel file')
+    parser.add_argument('dica_export', help='DHBA or DPCA export Excel data file')
+    parser.add_argument('dica_dd', help='DHBA or DPCA data dictionary')
+    parser.add_argument('output_dir', help='Output directory to store generated CSV files')
     args = parser.parse_args()
     cast_loader = loaders.CastorExportFileLoader(args.castor_export)
     cast_data = cast_loader.load()
