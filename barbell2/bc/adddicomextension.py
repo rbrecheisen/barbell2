15d1d3ac4cadeec245e8b78abdfc6e1ac65fbc98
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-09-26 08:51:17 +0200

    Saving work

--- barbell2/bc/adddicomextension.py
+++ barbell2/bc/adddicomextension.py
@@ -8,7 +8,7 @@ def main():
     parser = common.get_parser()
     args = parser.parse_args()
     if args.input_dir != args.output_dir and args.overwrite == 1:
-
+        pass
     for f in os.listdir(args.input_dir):
         print(f)
 
