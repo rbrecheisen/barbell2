57ae66731aeae5b37e5c0f4e6d9e329f6094930c
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-09-26 11:17:21 +0200

    Saving version 1.63.0 before deploying to PyPI. DICA loader reports audit

--- barbell2/castor/loaders.py
+++ barbell2/castor/loaders.py
@@ -22,6 +22,11 @@ class DicaExportFileLoader:
     def __init__(self, file_path_data, file_path_dict):
         self.file_path_data = file_path_data
         self.file_path_dict = file_path_dict
+        self.audit = None
+        if 'dpca' in self.file_path_data:
+            self.audit = 'DPCA'
+        if 'dhba' in self.file_path_data:
+            self.audit = 'DHBA'
         self.data = None
 
     def load(self):
@@ -29,16 +34,16 @@ class DicaExportFileLoader:
             raise RuntimeError('Data must be recent Excel format (*.xlsx)')
         if self.file_path_dict.endswith('.xls'):
             raise RuntimeError('Data dictionary must be recent format (*.xlsx)')
-        print('Loading DICA data...')
+        print(f'Loading {self.audit} data...')
         df_pat = pd.read_excel(self.file_path_data, dtype=str, sheet_name='patient', engine='openpyxl', index_col='uri')
         df_trt = pd.read_excel(self.file_path_data, dtype=str, sheet_name='verrichting', engine='openpyxl', index_col='uri')
         df_com = pd.read_excel(self.file_path_data, dtype=str, sheet_name='comorbiditeiten', engine='openpyxl', index_col='uri')
-        print('Loading DICA data dictionary...')
+        print(f'Loading {self.audit} data dictionary...')
         df_dict_pat = pd.read_excel(self.file_path_dict, dtype=str, sheet_name='patient', engine='openpyxl')
         df_dict_trt = pd.read_excel(self.file_path_dict, dtype=str, sheet_name='verrichting', engine='openpyxl')
         df_dict_com = pd.read_excel(self.file_path_dict, dtype=str, sheet_name='comorbiditeiten', engine='openpyxl')
-        if 'dpca' in self.file_path_data:
+        if self.audit == 'DPCA':
             self.data = data.DpcaData(df_pat, df_trt, df_com, df_dict_pat, df_dict_trt, df_dict_com)
-        if 'dhba' in self.file_path_data:
+        if self.audit == 'DHBA':
             self.data = data.DhbaData(df_pat, df_trt, df_com, df_dict_pat, df_dict_trt, df_dict_com)
         return self.data
