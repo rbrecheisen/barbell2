659dfbe2e8112b4e55a075b2b5176b9f14383422
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-12-20 14:21:29 +0100

    Saving version 2.11.0 before deploying to PyPI.

--- barbell2/imaging/dcm2nifti.py
+++ barbell2/imaging/dcm2nifti.py
@@ -5,9 +5,6 @@ import subprocess
 class Dicom2Nifti:
 
     def __init__(self, dcm_file_dir_or_path, nii_file_path):
-        result = subprocess.check_output('dcm2niix')
-        if result != 0:
-            raise Exception('Tool dcm2niix is not installed!')
         if os.path.isfile(dcm_file_dir_or_path):
             self.merge_option = '-m n'
         else:
