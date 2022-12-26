7f45f82ffe41b89fe8c14dcf524370b4d6bae2ff
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2022-12-26 18:36:14 +0100

    Create radiomics extractor component

--- barbell2/imaging/seg.py
+++ barbell2/imaging/seg.py
@@ -31,14 +31,38 @@ class TotalSegmentator:
 
 
 class RoiSliceExtractor:
-    """ Extracts all slices in the source image that intersect
-    the given ROI
+    """ Extracts all slices (as a list of 2D NumPy arrays) in the source NIFTI 
+    image that intersect the given ROI. Additionally, it outputs corresponding
+    array indexes and DICOM instance numbers, if available in the source image's
+    meta data. This requires that the NIFTI image was created from a DICOM series
+    using the meta-data option.
+
+    Outputs:
+    - List of 2D Numpy arrays
+    - List of z- or depth indexes pointing to the 2D arrays inside the volume
+    - (optional) List of DICOM instance numbers
      """
-    def __init__(self, image_path, roi_path):
-        self.image_path = image_path
+    def __init__(self, nifti_path, roi_path):
+        self.nifti_path = nifti_path
         self.roi_path = roi_path
 
     def execute(self):
+        pass
+
+
+class RoiRadiomicsExtractor:
+    """ Calculates number of radiomics features on the given ROI. 
+    """
+    def __init__(self, roi_path):
+        pass
+
+    def execute(self):
+        pass
+
+    def get_volume(self):
+        pass
+
+    def get_mean_pixel_value(self):
         pass
 
 
