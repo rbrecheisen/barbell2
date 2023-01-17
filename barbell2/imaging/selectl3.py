56790734d17ec7f1d35a661061af038aa5e7cba7
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-01-17 16:45:19 +0100

    Saving work

--- barbell2/imaging/selectl3.py
+++ barbell2/imaging/selectl3.py
@@ -8,16 +8,6 @@ import numpy as np
 class L3Selector:
 
     def __init__(self, dicom_series_dir, nifti_image, nifti_roi, alpha=0.68):
-        """ Searches for L3 image in given DICOM series folder based on the L3 vertebra segmentation. This is done
-        by first searching for the first and last image slices in the NIFTI image that intersect the NIFTI 
-        ROI (L3 vertebra). The image slice indices are then converted to Z-coordinates using the voxel-to-patient
-        coordinate transformation of the NIFTI header. To determine the exact L3 image for body composition the
-        :param dicom_series_dir: Folder containing all (and only) DICOM files for a given series
-        :param nifti_image: File path to NIFTI file created from DICOM series
-        :param nifti_roi: File path to NIFTI region-of-interest for segmented L3 vertebral body
-        :param alpha: 
-        :return: File path to the L3 image
-        """
         self.dicom_series_dir = dicom_series_dir
         self.nifti_image = nifti_image
         self.nifti_roi = nifti_roi
@@ -76,24 +66,27 @@ class L3Selector:
         i_min, i_max = self.get_min_max_slice_idx_nifti_roi(nifti_roi)
         nifti_image = nibabel.load(self.nifti_image)
         z_min, z_max = self.get_min_max_z_coord_patient_position_nifti_image(i_min, i_max, nifti_image)
-        if self.alpha == 'top-middle-bottom':
-            return []
-        elif self.alpha == 'all':
-            return []
+        if self.alpha == 'minmedianmax':
+            l3_paths = []
+            z_median = (z_max - z_min) / 2.0
+            for z in [z_min, z_median, z_max]:
+                l3_path = self.get_nearest_dicom_image_for_estimated_z_coord(self.dicom_series_dir, z)
+                l3_paths.append(l3_path)
+            return l3_paths
         else:
             z_estimated = self.calculate_estimated_z_coord_l3(z_min, z_max, self.alpha)
-            print(f'i_min: {i_min}, i_max: {i_max}, z_min: {z_min}, z_max: {z_max}, z_estimated: {z_estimated}')
             l3_path = self.get_nearest_dicom_image_for_estimated_z_coord(self.dicom_series_dir, z_estimated)
-            print(f'Found L3: {l3_path}')
+            print(f'{l3_path}')
             return l3_path
 
 
 if __name__ == '__main__':
     def main():
         selector = L3Selector(
-            '/Users/Ralph/Desktop/l3select/ct-abdomen',
-            '/Users/Ralph/Desktop/l3select/ct-abdomen.nii.gz',
-            '/Users/Ralph/Desktop/l3select/l3.nii.gz',
+            '/Users/Ralph/Desktop/auto-vs-manual-l3/scans_prep/al',
+            '/Users/Ralph/Desktop/auto-vs-manual-l3/scans_prep_nifti/al.nii.gz',
+            '/Users/Ralph/Desktop/auto-vs-manual-l3/segmentations/al/vertebrae_L3.nii.gz',
+            'minmedianmax',
         )
         x = selector.execute()
         print(x)
