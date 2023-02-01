8383825ff813a22f27c08ab0f6c67b22f3994161
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-02-01 15:20:51 +0100

    Saving work

--- barbell2/bodycomp/seg.py
+++ barbell2/bodycomp/seg.py
@@ -2,6 +2,10 @@ import os
 import json
 import zipfile
 import logging
+import pydicom
+import numpy as np
+
+from barbell2.utils import is_dicom_file, get_pixels
 
 logger = logging.getLogger(__name__)
 
@@ -44,6 +48,33 @@ class MuscleFatSegmentator:
                 logger.error(f'Unknown model file {file_name}')
         return model, contour_model, params
 
+    def normalize(img, min_bound, max_bound):
+        img = (img - min_bound) / (max_bound - min_bound)
+        img[img > 1] = 0
+        img[img < 0] = 0
+        c = (img - np.min(img))
+        d = (np.max(img) - np.min(img))
+        img = np.divide(c, d, np.zeros_like(c), where=d != 0)
+        return img
+
+    def predict_contour(self, contour_model, src_img, params):
+        ct = np.copy(src_img)
+        ct = self.normalize(ct, params['min_bound_contour'], params['max_bound_contour'])
+        img2 = np.expand_dims(ct, 0)
+        img2 = np.expand_dims(img2, -1)
+        pred = contour_model.predict([img2])
+        pred_squeeze = np.squeeze(pred)
+        pred_max = pred_squeeze.argmax(axis=-1)
+        mask = np.uint8(pred_max)
+        return mask
+
+    def convert_labels_to_157(prediction):
+        new_prediction = np.copy(prediction)
+        new_prediction[new_prediction == 1] = 1
+        new_prediction[new_prediction == 2] = 5
+        new_prediction[new_prediction == 3] = 7
+        return new_prediction
+
     def execute(self):
         if self.input_files is None:
             logger.error('Input files not specified')
@@ -55,6 +86,27 @@ class MuscleFatSegmentator:
             logger.error('Model files not specified')
             return None
         model, contour_model, params = self.load_model_files()
+        for f in self.input_files:
+            if is_dicom_file(f):
+                p = pydicom.dcmread(f)
+                img1 = get_pixels(p, normalize=True)
+                if contour_model is not None:
+                    mask = self.predict_contour(contour_model, img1, params)
+                    img1 = self.normalize(img1, params['min_bound'], params['max_bound'])
+                    img1 = img1 * mask
+                else:
+                    img1 = self.normalize(img1, params['min_bound'], params['max_bound'])
+                img1 = img1.astype(np.float32)
+                img2 = np.expand_dims(img1, 0)
+                img2 = np.expand_dims(img2, -1)
+                pred = model.predict([img2])
+                pred_squeeze = np.squeeze(pred)
+                print(pred_squeeze.shape)
+                pred_max = pred_squeeze.argmax(axis=-1)
+                pred_max = self.convert_labels_to_157(pred_max)
+                break
+            else:
+                logger.warning(f'File {f} is not a valid DICOM file')
 
 
 if __name__ == '__main__':
